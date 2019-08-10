888e647f4f985542b263da5f034c7a2c9e5c02d4
rokhuda <rokhuda@gmail.com>
2019-08-10 15:55:50 +0200

    added specs

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 global.counter = 0; //Keeps track of how many instances of the class are created
-let data;
-module.exports = class Visitor {
+
+
+class Visitor {
     constructor(fullName, age, dateOfVisit, timeOfVisit, comments, visitorAssistor) {
         this.fullName = fullName;
         this.age = age;
@@ -17,57 +18,36 @@ module.exports = class Visitor {
     save() {
         let data = JSON.stringify(this);
         let num = this.number;
-        fs.writeFileSync('Visitor_' + num + '.json', data);
+        try {
+            
+        } catch (error) {
+            
+        }
+        fs.writeFileSync('visitor_' + num + '.json', data);
 
-        console.log(data);
-        
         return "File has been created."
 
+}
+  //loads
+load() {
+    let data = JSON.stringify(this)
+    try {
+        fs.readFileSync(path, data)
+        } catch (err) {
+            
+        return data
+
     }
-
-
-    //Takes in number and returns visitor associated with the number
-    load(num) {
-
-        fs.readdir(process.cwd(), handleFiles); ///Read files in the folder    (Process.cwd() = current working directory)
-        var filename;
-        var i;
-
-        function handleFiles(err, files) {
-            for (i = 0; i < files.length; ++i) {
-                filename = files[i];
-                if (filename.match(num)) //Match filename with strings(num)
-                {
-                    //Open the file and get the name:
-                    fs.readFile(filename, (err, data) => {
-                        if (err) throw err;
-                        let guest = JSON.parse(data);
-                        console.log(guest);
-                        return guest;
-                    });
-                }
-            }
-        }
-        return num;
-    }
+    
+}
 }
 
-// visitor = new Visitor('Rokhuda', 12, "21/3/19", "1hr30", 'Shinigami love apples', 'Ryuk');
-// visitor2 = new Visitor('Light', 12, "21/5/19", "3hr00", 'Shinigami love apples', 'Ryuk');
-// visitor3 = new Visitor('Rem', 12, "21/5/19", "4hr00", 'Shinigami love apples', 'Ryuk');
+let visitor_1 = new Visitor('Rokhuda Tshitimbi', 30, "21/3/19", "1hr30", 'attentive', 'Ryuk');
+let visitor_2 = new Visitor('Light Yagami', 19, "21/5/19", "3hr00", 'has potential', 'Rem');
 
-/*  save(visitor); */
-// save(visitor2);
-// save(visitor3);
-// console.log(visitor.save());
-// console.log(visitor.load(1))
-// console.log(visitor.save());
-// console.log(visitor.load(2))
-// console.log(visitor.save());
-// console.log(visitor.load(3))
+console.log(visitor_1.save())
+console.log(visitor_2.save())
+console.log(visitor_1.load())
+console.log(visitor_2.load())
 
-
-//Testing
-// load(1);
-// load(2);
-// load(3); 
+module.exports = Visitor;
--- spec/main_spec.js
+++ spec/main_spec.js
@@ -1,10 +1,9 @@
-//
-
 describe("Save method", () => {
-    var Visitor;
+    
     beforeEach(function () {
+        var Visitor;
         Visitor = require('../main');
-        visitor = new Visitor('Rokhuda', 12, "21/3/19", "1hr30", 'Shinigami love apples', 'Ryuk');
+        visitor = new Visitor('Rokhuda', 30, "21/3/19", "1hr30", 'attentive', 'Ryuk');
     });
 
     it("should check if save method is defined", () => {
@@ -22,14 +21,24 @@ describe("Load method", () => {
     beforeEach(function () {
         var Visitor;
         Visitor = require('../main');
-        visitor = new Visitor('Rokhuda', 12, "21/3/19", "1hr30", 'Shinigami love apples', 'Ryuk');
+        visitor = new Visitor('Rokhuda', 30, "21/3/19", "1hr30", 'attentive', 'Ryuk');
     });
 
     it("should check if the load method is defined.", () => {
         expect(visitor.load(1)).toBeDefined();
     });
 
+    it("should take in a number and load a visitor object file", () => {
+        
+        let result = JSON.stringify(visitor)
+        expect(visitor.load(1)).toEqual(result);
+    });
 });
 
-
-
+describe("visitor count", () => {
+    it("should assign every instance of a visitor a number", () => {
+        global.count = 0
+        let result = counter++ 
+        expect(result).toBeGreaterThan(0)
+    });
+});  
